543d1dc5493578fdf0f5e02cd004e33fce2f345a
Mat Mariani <someone.mariani@gmail.com>
2024-11-14 14:54:32 -0500

    Added lissajous curves to platformer

--- examples/platformer/src/game.cpp
+++ examples/platformer/src/game.cpp
@@ -14,6 +14,26 @@ namespace {
 	constexpr float transition_duration = 0.4f;
 	constexpr int width = 240;
 	constexpr int height = 135;
+
+	// sweetie16 palette
+	unsigned int pal[] = {
+			0xff1a1c2c,
+			0xff5d275d,
+			0xffb13e53,
+			0xffef7d57,
+			0xffffcd75,
+			0xffa7f070,
+			0xff38b764,
+			0xff257179,
+			0xff29366f,
+			0xff3b5dc9,
+			0xff41a6f6,
+			0xff73eff7,
+			0xfff4f4f4,
+			0xff94b0c2,
+			0xff566c86,
+			0xff333c57,
+	};
 }// namespace
 
 #define XPOS(x) (x * entity::EN_GRID_SIZE)
@@ -73,14 +93,44 @@ void game::update() {
 	entity::update();
 }
 
+float t = 0.0f;
 void game::render() {
-	pti_cls(0xffef7d57);
+	// pti_cls(0xffef7d57);
 
 	/* adjust camera */
 	int cam_x, cam_y;
 	pti_get_camera(&cam_x, &cam_y);
 	pti_camera(_pti_max(0, _pti_min(entity::EN_ROOM_WIDTH - width, cam_x)),
 			   _pti_max(0, _pti_min(entity::EN_ROOM_HEIGHT - height, cam_y)));
+
+	//>> lissajous curves:
+	float cx1 = (width * 0.5f) - sinf(t / 3) * (width * 0.5f);
+	float cy1 = (height * 0.5f) - cosf(t / 2) * (height * 0.5f);
+	float cx2 = (width * 0.5f) + sinf(t / 3) * (width * 0.5f);
+	float cy2 = (height * 0.5f) + cosf(t / 2) * (height * 0.5f);
+	t += (1 / 30.0f);
+
+	int x, y;
+	for (y = 0; y < height; y++) {
+		float dy1 = (y - cy1) * (y - cy1);
+		float dy2 = (y - cy2) * (y - cy2);
+		for (x = 0; x < width; x++) {
+			float dx1 = (x - cx1) * (x - cx1);
+			float dx2 = (x - cx2) * (x - cx2);
+			int c = (((int) sqrtf(dx1 + dy1) ^ (int) sqrtf(dx2 + dy2)) >> 4);
+			if ((c & 1) == 0) {
+				if (rand() % 5 == 1) {
+					pti_pset(x, y, ((unsigned long int) pal[1] << 32) | pal[2]);
+				}
+			} else {
+				if (rand() % 5 == 1) {
+					pti_pset(x, y, ((unsigned long int) pal[3] << 32) | pal[4]);
+				}
+			}
+		}
+	}
+
+	//>> =============
 
 	/* draw tilemap */
 	// pti_plot_tilemap(0, 0);
