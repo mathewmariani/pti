0e1fcb481c1076881182c6e6821427af5223f424
Mat Mariani <someone.mariani@gmail.com>
2024-11-19 15:42:44 -0500

    Updated cmake project

--- extra/cmake/sokol.cmake
+++ extra/cmake/sokol.cmake
@@ -10,13 +10,6 @@ if(BUILD_WITH_SOKOL)
     find_package(Threads REQUIRED)
   endif()
 
-  #defines
-  if (CMAKE_SYSTEM_NAME STREQUAL Emscripten)
-    add_definitions(-DSOKOL_GLES3)
-  elseif (CMAKE_SYSTEM_NAME STREQUAL Darwin)
-    add_definitions(-DSOKOL_GLCORE)
-  endif()
-
   set(SOKOL_DIR ${THIRDPARTY_DIR}/sokol)
   set(SOKOL_HEADERS
     ${SOKOL_DIR}/sokol.c
@@ -26,17 +19,18 @@ if(BUILD_WITH_SOKOL)
 
   add_library(sokol STATIC ${SOKOL_HEADERS})
 
-  if(CMAKE_SYSTEM_NAME STREQUAL Darwin)
-    # compile sokol.c as Objective-C
+  if (CMAKE_SYSTEM_NAME STREQUAL Emscripten)
+    target_compile_definitions(sokol PRIVATE SOKOL_GLES3)
+  elseif (CMAKE_SYSTEM_NAME STREQUAL Darwin)
+    target_compile_definitions(sokol PRIVATE SOKOL_GLCORE)
     target_compile_options(sokol PRIVATE -x objective-c)
     target_link_libraries(sokol
         "-framework QuartzCore"
         "-framework Cocoa"
         "-framework OpenGL")
-  else()
-    if (CMAKE_SYSTEM_NAME STREQUAL Linux)
-      target_link_libraries(sokol INTERFACE X11 Xi Xcursor GL asound dl m)
-      target_link_libraries(sokol PUBLIC Threads::Threads)
-    endif()
+  elseif (CMAKE_SYSTEM_NAME STREQUAL Linux)
+    target_compile_definitions(sokol PRIVATE SOKOL_GLCORE)
+    target_link_libraries(sokol INTERFACE X11 Xi Xcursor GL asound dl m)
+    target_link_libraries(sokol PUBLIC Threads::Threads)
   endif()
 endif()
