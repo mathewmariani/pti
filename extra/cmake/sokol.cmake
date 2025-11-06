19b5233d8b4ae595332814fe191c3094f7ff62ea
mathewmariani <someone.mariani@gmail.com>
2025-11-05 22:40:32 -0500

    Added opengl platform

--- extra/cmake/sokol.cmake
+++ extra/cmake/sokol.cmake
@@ -4,6 +4,7 @@ option(BUILD_WITH_SOKOL "sokol Enabled" ${BUILD_WITH_SOKOL_DEFAULT})
 message("BUILD_WITH_SOKOL: ${BUILD_WITH_SOKOL}")
 
 if(BUILD_WITH_SOKOL)
+  #defines
   if (CMAKE_SYSTEM_NAME STREQUAL Emscripten)
     add_definitions(-DSOKOL_GLES3)
   elseif (CMAKE_SYSTEM_NAME STREQUAL Darwin)
@@ -19,7 +20,7 @@ if(BUILD_WITH_SOKOL)
   set(SOKOL_SRC ${SOKOL_DIR}/sokol.c)
 
   add_library(sokol STATIC ${SOKOL_SRC})
-  target_include_directories(sokol PUBLIC ${SOKOL_DIR})
+  target_include_directories(sokol INTERFACE ${SOKOL_DIR})
 
   if(CMAKE_SYSTEM_NAME STREQUAL Darwin)
     # compile sokol.c as Objective-C
@@ -30,7 +31,7 @@ if(BUILD_WITH_SOKOL)
         "-framework OpenGL")
   else()
     if (CMAKE_SYSTEM_NAME STREQUAL Linux)
-      target_link_libraries(sokol PUBLIC X11 Xi Xcursor GLX GL dl m)
+      target_link_libraries(sokol INTERFACE X11 Xi Xcursor GL asound dl m)
       target_link_libraries(sokol PUBLIC Threads::Threads)
     endif()
   endif()
